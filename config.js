cad2558389bd28cdc597f490be5d623086a82d2d
Shehan2007su <142976093+Shehan2007su@users.noreply.github.com>
2023-08-23 14:05:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['0713856912', 'shehan', true],
+  ['0721371841', 'SA', true], 
   [''] 
 ] //Number of owners
 
